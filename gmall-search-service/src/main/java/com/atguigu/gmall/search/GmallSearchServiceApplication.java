c1693e969d626c5d550ae7b9a0dbb1ed99d57e04
fanxiaoxiang <fanxiaoxiang3988@163.com>
2019-08-06 10:41:24 +0800

    springboot整合es并测试成功

---
+++ gmall-search-service/src/main/java/com/atguigu/gmall/search/GmallSearchServiceApplication.java
@@ -0,0 +1,21 @@
+package com.atguigu.gmall.search;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+
+/**
+ * gmall-search-service依赖了gmall-serviceutil，serviceutil导入了springboot的jdbc相关的依赖，所以search-service会自动引入jdbc相关的配置
+ * 有三种解决方法：
+ *      1、启动类标注exclude = DataSourceAutoConfiguration.class
+ *      2、在pom文件中排除jdbc相关的依赖
+ *      3.application中添加数据库配置（用不到mysql数据库，不考虑）
+ */
+@SpringBootApplication(exclude = DataSourceAutoConfiguration.class)
+public class GmallSearchServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GmallSearchServiceApplication.class, args);
+    }
+
+}
